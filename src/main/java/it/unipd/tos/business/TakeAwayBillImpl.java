af235866a16c88e2834cc1b1df3a225d540855fe
Massimo <maxdr64@gmail.com>
2020-12-07 11:29:02 +0100

    close #3

--- src/main/java/it/unipd/tos/business/TakeAwayBillImpl.java
+++ src/main/java/it/unipd/tos/business/TakeAwayBillImpl.java
@@ -17,6 +17,7 @@ public double getOrderPrice(List<MenuItem> itemsOrdered, User user) throws TakeA
 double total = 0;
 double min = 1000;
 int gelati = 0;
+int budini = 0;
 if(itemsOrdered == null) {throw new TakeAwayBillException("Lista nulla");
 }
 if(itemsOrdered.isEmpty()) {
@@ -26,13 +27,25 @@ throw new TakeAwayBillException("Lista ordini vuota");
 for (MenuItem item : itemsOrdered) {double current = item.getPrice();
 if(item.getType().equals(ItemType.Gelato)) {
 gelati++;
-}if(current<min) {
+}
+else if(item.getType().equals(ItemType.Budino)) {
+gelati++;
+}
+if(current<min) {
 min=current;
 }
 total += current;
 }
+//sconto 50%
 if(gelati > 5) {
 total -= 0.5*min;   
+}
+
+//sconto 10%
+if(total>50) {
+if(budini>0 || gelati >0) {
+total -= total*0.1;
+}
 }
 return total;
 }
